922ec1287858d01fad2c2829c52567fd46172271
KeyronSmith2025 <98052525+KeyronSmith2025@users.noreply.github.com>
2024-03-22 23:52:56 +0000

    Thrid times the charm??

--- testadd.js
+++ testadd.js
@@ -41,8 +41,8 @@ app.get('/register', (req, res) => {
     res.send(`
         <h2>Register</h2>
         <form action="/register" method="post">
-            <input type="text" name="userId" placeholder="User ID" required><br>
-            <input type="password" name="password" placeholder="Password" required><br>
+            <input type="text" name="UserId" placeholder="User ID" required><br>
+            <input type="password" name="UserPass" placeholder="Password" required><br>
             <button type="submit">Register</button>
         </form>
     `);
@@ -67,8 +67,8 @@ app.get('/login', (req, res) => {
     res.send(`
         <h2>Login</h2>
         <form action="/login" method="post">
-            <input type="text" name="userId" placeholder="User ID" required><br>
-            <input type="password" name="password" placeholder="Password" required><br>
+            <input type="text" name="UserID" placeholder="User ID" required><br>
+            <input type="password" name="UserPass" placeholder="Password" required><br>
             <button type="submit">Login</button>
         </form>
     `);
